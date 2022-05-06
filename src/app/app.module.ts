90232c872678102dd78958c3f1a9605f683f9738
Mariem Bokri <mariem.bokri@esprit.tn>
2022-05-06 21:54:33 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChampionComponent } from './champion/champion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionComponent
   ],
   imports: [
     BrowserModule,
